52994de05d148ee708808f9aef0b139b7e807eda
Vinh <vinhbuith32412000@gmail.com>
2022-08-10 17:46:26 +0700

    Bugs happen when use minus button, aparently can't render plus button when push minus button or delete button first

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -8,42 +8,71 @@ class HomesController < ApplicationController
   end
 
   def add_order_to_cart 
-    selected_shoe = Shoe.find(params[:shoe_id])
+    @selected_shoe = Shoe.find(params[:shoe_id])
     current_cart = Cart.first 
     
-    Order.create(shoe_id: selected_shoe.id, cart_id: current_cart.id, quantity: 1)
+    @order = Order.create(shoe_id: @selected_shoe.id, cart_id: current_cart.id, quantity: 1)
 
-    selected_shoe.selected = true 
-    selected_shoe.save!
+    @selected_shoe.selected = true 
+    @selected_shoe.save!
+
+    @amount = current_cart.amount
+    @orders = current_cart.orders
+
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   def remove_order_from_cart 
-    selected_shoe = Shoe.find(params[:shoe_id])
+    @selected_shoe = Shoe.find(params[:shoe_id])
     current_cart = Cart.first 
 
-    Order.where(shoe_id: selected_shoe.id, cart_id: current_cart.id).destroy_all
+    Order.where(shoe_id: @selected_shoe.id, cart_id: current_cart.id).destroy_all
 
-    selected_shoe.selected = false 
-    selected_shoe.save!
+    @selected_shoe.selected = false 
+    @selected_shoe.save!
+
+    @amount = current_cart.amount
+    @orders = current_cart.orders
+
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   def increase_order_quantity 
-    order = Order.find(params[:order_id])
-    order.quantity += 1
-    order.save!
+    @order = Order.find(params[:order_id])
+    @order.quantity += 1
+    @order.save!
+    
+    current_cart = Cart.first 
+    @amount = current_cart.amount
+
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   def decrease_order_quantity 
-    order = Order.find(params[:order_id])
+    @order = Order.find(params[:order_id])
+    @selected_shoe = @order.shoe
 
-    if order.quantity == 1 
-      selected_shoe = order.shoe
-      selected_shoe.selected = false 
-      selected_shoe.save!
-      order.destroy
+    if @order.quantity == 1 
+      @selected_shoe.selected = false 
+      @selected_shoe.save!
+      @order.destroy
     else
-      order.quantity -= 1
-      order.save! 
+      @order.quantity -= 1
+      @order.save! 
+    end
+
+    current_cart = Cart.first 
+    @amount = current_cart.amount
+    @orders = current_cart.orders
+
+    respond_to do |format|
+      format.turbo_stream
     end
   end
 end
